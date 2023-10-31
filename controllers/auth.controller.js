cd6b55da0134e41c691e0720a149b139da10eaee
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-10-31 23:27:32 +0100

    add verifyUser

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,17 +1,47 @@
 const User = require("../models/usersSchema");
+const Token = require("../models/token");
 const jsonwebtoken = require("jsonwebtoken");
-
+const crypto = require("crypto");
+const sendEmail = require("../mailer/email");
 
 const createUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const user = new User({ name, email });
     await User.register(user, password);
+
+    const token = await new Token({
+      userId: user._id,
+      token: crypto.randomBytes(32).toString("hex"),
+    }).save();
+
+    const message = `${process.env.BASE_URL}/user/verify/${user.id}/${token.token}`;
+    await sendEmail(user.email, "Verify Email", message);
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     res
       .status(500)
       .json({ error: "An error occurred while creating the user" });
+    console.log(error);
+  }
+};
+
+const verifyUser = async (req, res) => {
+  try {
+    const token = await Token.findOne({
+      userId: user._id,
+      token: req.params.token,
+    });
+    await User.findOneAndUpdate(
+      { _id: req.params.id },
+      { verified_status: true }
+    );
+    await Token.findByIdAndRemove(token._id);
+    res.send("email verified sucessfully");
+  } catch (error) {
+    const user = await User.findOne({ _id: req.params.id });
+    res.status(400).send("Error");
+    console.log(user._id);
     console.log(error);
   }
 };
@@ -31,5 +61,6 @@ const loginUser = async (req, res) => {
 
 module.exports = {
   createUser,
+  verifyUser,
   loginUser,
 };
