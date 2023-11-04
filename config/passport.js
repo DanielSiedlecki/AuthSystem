80cd2d4e7f3da9bbaeac70cbf0b036c1046735b1
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-04 15:56:30 +0100

    add facebook auth config

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,35 @@
 const passport = require("passport");
 const User = require("../models/usersSchema");
+const FacebookStrategy = require("passport-facebook").Strategy;
 
 module.exports = () => {
   passport.use(User.createStrategy());
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FB_APP_ID,
+        clientSecret: process.env.APP_SECRET,
+        callbackURL: process.env.REDIRECT_URI,
+        profileFields: ["email", "name"],
+      },
+      function (accessToken, refreshToken, profile, done) {
+        const userData = {
+          facebook_id: profile.id,
+          name: profile.name.givenName,
+          email: profile.email || "email-not-provided",
+        };
+
+        new User(userData).save();
+        console.log(profile._json);
+        done(null, profile);
+      }
+    )
+  );
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+  });
 };
