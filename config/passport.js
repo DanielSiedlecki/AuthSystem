49e3d6ed823e3d6852d853060fde452e19190a1e
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-08 13:43:34 +0100

    refactor passport strategy

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,5 @@
 const passport = require("passport");
+const authController = require("../controllers/auth.controller");
 const User = require("../models/usersSchema");
 const FacebookStrategy = require("passport-facebook").Strategy;
 
@@ -12,16 +13,29 @@ module.exports = () => {
         callbackURL: process.env.REDIRECT_URI,
         profileFields: ["email", "name"],
       },
-      function (accessToken, refreshToken, profile, done) {
-        const userData = {
-          facebook_id: profile.id,
-          name: profile.name.givenName,
-          email: profile.email || "email-not-provided",
-        };
 
-        new User(userData).save();
-        console.log(profile._json);
-        done(null, profile);
+      function (req, accessToken, refreshToken, profile, done) {
+        process.nextTick(async function () {
+          try {
+            const localuser = await User.findOne({
+              email: profile._json.email,
+            });
+            if (localuser) {
+              console.log("User found");
+              done(null, localuser);
+            } else {
+              let new_user = {
+                facebook_id: profile.id,
+                name: profile._json.first_name + profile._json.last_name,
+                email: profile._json.email,
+              };
+              const newUser = await new User(new_user).save();
+              done(null, newUser);
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        });
       }
     )
   );
